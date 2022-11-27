43d634af5295675e30b884b6bce19f791a48151c
SnowingFox <1503401882@qq.com>
2022-11-27 09:14:52 +0000

    Deploying to gh-pages from @ hfut-soft-ware/hfut-space-app@15c4f058d95733d56abdee3cb70897365e490373 🚀

---
+++ assets/pages-test-index.bcd0996f.js
@@ -0,0 +1,1 @@
+import{o as e,g as s,j as r,k as a}from"./index.a8d9b4eb.js";import{_ as n,a as o}from"./sys.14d446b4.js";const t={};function d(n,t){const d=r(a("sys"),o);return e(),s(d)}(0,Object.defineProperty)(d,"name",{value:"_sfc_render",configurable:!0});var f=n(t,[["render",d]]);export{f as default};
